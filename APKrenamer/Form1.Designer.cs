7586f4d25abba774b7b5e3ba7779ac6550d2dc38
adrianvic <adrianvic.personal@outlook.com>
2023-09-20 18:40:08 -0300

    First commit

---
+++ APKrenamer/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace APKrenamer
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainWindow));
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.btStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btFile = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.lbTitle.Location = new System.Drawing.Point(5, 9);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(186, 37);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "apk renamer ;)";
+            // 
+            // btStart
+            // 
+            this.btStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btStart.Location = new System.Drawing.Point(111, 249);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(49, 23);
+            this.btStart.TabIndex = 1;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(12, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(260, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Files:";
+            // 
+            // btFile
+            // 
+            this.btFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btFile.Location = new System.Drawing.Point(166, 249);
+            this.btFile.Name = "btFile";
+            this.btFile.Size = new System.Drawing.Size(78, 23);
+            this.btFile.TabIndex = 3;
+            this.btFile.Text = "Select file";
+            this.btFile.UseVisualStyleBackColor = true;
+            this.btFile.Click += new System.EventHandler(this.btFile_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(12, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(260, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "%name% %ver%";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.ForeColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(12, 114);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(260, 129);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "-=LOG=-";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 253);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(93, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Ignore errors";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(249, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(23, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 281);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btFile);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.lbTitle);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainWindow";
+            this.Opacity = 0.9D;
+            this.ShowIcon = false;
+            this.Text = "APK Renamer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btFile;
+        private System.Windows.Forms.TextBox textBox2;
+        internal System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ APKrenamer/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Csharp.Apk_Reader;
+
+namespace APKrenamer
+{
+    public partial class mainWindow : Form
+    {
+        // Initializing variables
+        List<string> path = new List<string>();
+        string diagCurrentFileName;
+
+        // mainwindow stuff
+        public mainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // On btStart click
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            // initialize a new instance of Apk_Reader as var "read"
+            var read = new Apk_Reader();
+            // Create "output" folder
+            Directory.CreateDirectory("./output");
+            // For every file on path...
+            for (int i = path.Count - 1; i >= 0; i--)
+            {
+                // ...try to pass it to ApkInfo and save stuff on variables
+                try
+                {
+                    string filename = path[i];
+                    diagCurrentFileName = filename;
+                    ApkInfo file_info = read.Get_File_Info(filename);
+                    string apkName = file_info.apkName;
+                    string versionName = file_info.versionName;
+                    string packageName = file_info.packageName;
+                    string sdkVersion = file_info.sdkVersion;
+                    string targetSdkVersion = file_info.targetSdkVersion;
+                    string versionCode = file_info.versionCode;
+                    string output = textBox2.Text;
+                    // Replace the variables with it`s content
+                    output = output.Replace("%name%", apkName).Replace("%ver%", versionName).Replace("%package%", packageName)
+             .Replace("%sdk%", sdkVersion).Replace("%vercode%", versionCode).Replace("%sdk%", sdkVersion).Replace("%target%", targetSdkVersion);
+                    log("File name: " + output);
+                    // Copy file with the new filename
+                    File.Copy(filename, "./output/" + output + ".apk");
+                }
+                // In case of exception, ignore it just sending a messagebox
+                catch (System.Exception)
+                {
+                    // Check if it should ignore the error
+                    if (checkBox1.Checked)
+                    {
+                        log("An error ocurred with" + diagCurrentFileName + ", but was ignored!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("An error ocurred while copying " + diagCurrentFileName + ", please verify if the filename is valid or try removing special characters from the original filename");
+                    }
+ 
+                }
+            }
+        }
+
+        // On btFile click
+        private void btFile_Click(object sender, EventArgs e)
+        {
+            // Shows a OpenFileDialog named openFileDialog
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // Default dir
+                openFileDialog.InitialDirectory = "c:\\";
+                // Filters for apk or other files
+                openFileDialog.Filter = "Android Package Kit (*.apk)|*.apk|Any file with a valid manifest (*.*)|*.*";
+                // Allows user to select more than 1 file
+                openFileDialog.Multiselect = true;
+
+                // Checks if the user selected a file
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // For every selected file...
+                    for (int i = 0; i < openFileDialog.FileNames.Length; i++)
+                    {
+                        // Add it to path
+                        path.Add(openFileDialog.FileNames[i]);
+                        // Add it to the directory textbox
+                        textBox1.AppendText(" " + openFileDialog.FileNames[i]);
+                    }
+                }
+            }
+        }
+
+        // For convenience sake, it adds a new line on log
+        public void log(string message)
+        {
+            textBox3.AppendText(Environment.NewLine + message);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Variables:\n%target% - Target SDK version\n%sdk% - Minimum SDK version\n%package% - Package name\n%name% - Application name\n%ver% - Application version\n%vercode% - Version code");
+        }
+    }
+}
